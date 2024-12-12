6aac2db56ce831089c70896d64341aa9ee2750bd
Whojoo <de.gier.robin@gmail.com>
2024-12-12 13:10:20 +0100

    feat: day 5 with dotnet instead

---
+++ 2024/day5/AoC2024.Day5/Handler.cs
@@ -0,0 +1,127 @@
+using System.Runtime.CompilerServices;
+
+namespace AoC2024.Day5;
+
+public static class Handler
+{
+    public static int CalculatePart1(List<string> input)
+    {
+        var (rawRules, rawUpdates) = SplitInput(input);
+        var rules = ParseRules(rawRules);
+        var updates = ParseUpdates(rawUpdates);
+
+        var sum = updates
+            .Where(update => IsValidUpdate(update, rules))
+            .Sum(update => update[update.Count / 2]);
+
+        return sum;
+    }
+
+    public static int CalculatePart2(List<string> input)
+    {
+        var (rawRules, rawUpdates) = SplitInput(input);
+        var rules = ParseRules(rawRules);
+        var updates = ParseUpdates(rawUpdates);
+
+        var sum = updates
+            .Where(update => !IsValidUpdate(update, rules))
+            .Select(update => update.OrderByRule(rules))
+            .Sum(update => update[update.Count / 2]);
+
+        return sum;
+    }
+
+    private static List<int> OrderByRule(this List<int> update, Dictionary<int, ISet<int>> rules) =>
+        update
+            .Order(Comparer<int>.Create((x, y) =>
+            {
+                if (rules.GetSetForKey(x).Contains(y))
+                {
+                    return 1;
+                }
+
+                if (rules.GetSetForKey(y).Contains(x))
+                {
+                    return -1;
+                }
+
+                return 0;
+            }))
+            .ToList();
+
+    private static bool IsValidUpdate(List<int> update, Dictionary<int, ISet<int>> rules)
+    {
+        var isValid = true;
+
+        for (var i = update.Count - 1; i > 0; i--)
+        {
+            var updatePage = update[i];
+            isValid = isValid && !update.Take(i).Any(rules.GetSetForKey(updatePage).Contains);
+        }
+
+        return isValid;
+    }
+
+    private static (List<string> rawRules, List<string> rawUpdates) SplitInput(List<string> input)
+    {
+        var splitIndex = input.IndexOf(string.Empty);
+
+        return (input.Take(splitIndex).ToList(), input.Skip(splitIndex + 1).ToList());
+    }
+
+    private static Dictionary<int, ISet<int>> ParseRules(List<string> rawRules)
+    {
+        var rules = new Dictionary<int, ISet<int>>();
+
+        foreach (var rule in rawRules)
+        {
+            var values = rule.Split("|").Select(int.Parse).ToArray();
+            var set = rules.GetSetForKey(values[0]);
+            set.Add(values[1]);
+        }
+
+        return rules;
+    }
+
+    private static List<List<int>> ParseUpdates(List<string> rawUpdates) =>
+        rawUpdates
+            .Select(update => update
+                .Split(",")
+                .Select(int.Parse)
+                .ToList())
+            .ToList();
+
+    private static ISet<int> GetSetForKey(this Dictionary<int, ISet<int>> dictionary, int key)
+    {
+        if (dictionary.TryGetValue(key, out var value))
+        {
+            return value;
+        }
+
+        value = new HashSet<int>();
+        dictionary.Add(key, value);
+
+        return value;
+    }
+
+    private static List<int> MoveItem(this List<int> list, int oldIndex, int newIndex)
+    {
+        var newList = new List<int>(list.Count);
+        foreach (var (v, i) in list.Select((v, i) => (v, i)))
+        {
+            if (i == oldIndex)
+            {
+                break;
+            }
+
+            if (i == newIndex)
+            {
+                newList.Add(list[oldIndex]);
+            }
+
+            newList.Add(v);
+        }
+
+        return newList;
+    }
+}
---
+++ 2024/day5/AoC2024.Day5/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using AoC2024.Day5;
+
+var input = Console.In.ReadInput();
+
+var first = Handler.CalculatePart1(input);
+var second = Handler.CalculatePart2(input);
+
+Console.WriteLine(first);
+Console.WriteLine(second);
+
+internal static class ConsoleExtensions
+{
+    public static List<string> ReadInput(this TextReader input)
+    {
+        List<string> output = [];
+
+        var whitespaceAllowed = true;
+
+        while (input.ReadLine() is { } line)
+        {
+            if (string.IsNullOrWhiteSpace(line) && !whitespaceAllowed)
+            {
+                break;
+            }
+
+            if (string.IsNullOrWhiteSpace(line))
+            {
+                whitespaceAllowed = false;
+            }
+            
+            Console.WriteLine(line);
+            output.Add(line);
+        }
+
+        return output;
+    }
+}
