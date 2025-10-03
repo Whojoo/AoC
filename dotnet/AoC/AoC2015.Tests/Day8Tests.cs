14e78f95f6f987d8a94e3df1f3e30a379c87518f
Robin de Gier <de.gier.robin@gmail.com>
2025-10-03 23:22:24 +0200

    feat: add 2025 day 8

---
+++ dotnet/AoC/AoC2015.Tests/Day8Tests.cs
@@ -0,0 +1,70 @@
+﻿namespace AoC2015.Tests;
+
+public class Day8Tests
+{
+  [Fact]
+  public void PartOneTest()
+  {
+    // Arrange
+    var input = File.ReadAllLines("test-input/day8.txt");
+    const int expectedResult = 15;
+    
+    // Act
+    var result = Day8.PartOne(input);
+    
+    // Assert
+    Assert.Equal(expectedResult, result);
+  }
+
+  [Theory]
+  [InlineData("\"\"", 2)]                         // ""
+  [InlineData("\"\\\"\"", 3)]                     // "\""
+  [InlineData("\"\\\\\"", 3)]                     // "\\"
+  [InlineData("\"\\x24\"", 5)]                    // "\x24"
+  [InlineData("\"ecn\\x50ooprbstnq\"", 5)]        // "ecn\x50ooprbstnq"
+  // Ignore the hex on \\x
+  [InlineData("\"x\\\"\\xcaj\\\\xwwvpdldz\"", 7)] // "x\"\xcaj\\xwwvpdldz"
+  // Do not ignore hex on \\\x, but also do not double count the \\
+  [InlineData("\"fdan\\\\\\x9e\"", 6)]            // "fdan\\\x9e"
+  public void PartOneTestEscapes(string input, int expectedResult)
+  {
+    // Arrange
+    // Act
+    var result = Day8.PartOne([input]);
+
+    // Assert
+    Assert.Equal(expectedResult, result);
+  }
+  
+  [Fact]
+  public void PartTwoTest()
+  {
+    // Arrange
+    var input = File.ReadAllLines("test-input/day8.txt");
+    const int expectedResult = 25;
+    
+    // Act
+    var result = Day8.PartTwo(input);
+    
+    // Assert
+    Assert.Equal(expectedResult, result);
+  }
+
+  [Theory]
+  [InlineData("\"\"", 4)]                         // "" -> "\"\""
+  [InlineData("\"\\\"\"", 6)]                     // "\"" -> "\"\\\"\""
+  [InlineData("\"\\\\\"", 6)]                     // "\\" -> "\"\\\\\""
+  [InlineData("\"\\x24\"", 5)]                    // "\x24" -> "\"\\x24\""
+  // [InlineData("\"ecn\\x50ooprbstnq\"", 5)]        // "ecn\x50ooprbstnq"
+  // [InlineData("\"x\\\"\\xcaj\\\\xwwvpdldz\"", 7)] // "x\"\xcaj\\xwwvpdldz"
+  // [InlineData("\"fdan\\\\\\x9e\"", 6)]            // "fdan\\\x9e"
+  public void PartTwoTestSeparateTestInput(string input, int expectedResult)
+  {
+    // Arrange
+    // Act
+    var result = Day8.PartTwo([input]);
+
+    // Assert
+    Assert.Equal(expectedResult, result);
+  }
+}
---
+++ dotnet/AoC/AoC2015/Day8.cs
@@ -0,0 +1,68 @@
+﻿using System.Resources;
+
+namespace AoC2015;
+
+public static class Day8
+{
+  public static void Run()
+  {
+    var lines = File.ReadAllLines("input/day8.txt");
+
+    Console.WriteLine($"Part one: {PartOne(lines)}");
+    Console.WriteLine($"Part two: {PartTwo(lines)}");
+  }
+
+  public static int PartOne(string[] input)
+  {
+    var totalInMemoryCharacters = input
+      .Select(line =>
+      {
+        var quotes = CountOccurrences("\"", line);
+        var escapedBackslash = CountOccurrences(@"\\", line);
+        var escapedHex = CountOccurrences(@"\x", line);
+
+        var cheatBackslash = CountOccurrences(@"\\\", line);
+        var counterCheatBackslash = CountOccurrences(@"\\\\", line);
+        
+        var cheatHex = CountOccurrences(@"\\x", line);
+        var counterCheatHex = CountOccurrences(@"\\\x", line);
+
+        escapedBackslash -= cheatBackslash - counterCheatBackslash;
+        escapedHex -= cheatHex - counterCheatHex;
+
+        return line.Length - quotes - escapedBackslash - escapedHex * 3;
+      })
+      .Sum();
+
+    var totalCharacters = input.Select(line => line.Length).Sum();
+
+    return totalCharacters - totalInMemoryCharacters;
+  }
+
+  public static int PartTwo(string[] input) =>
+    input
+      .Select(line =>
+      {
+        const int defaultIncrease = 2;
+        var quotes = CountOccurrences("\"", line);
+        var backslashes = CountOccurrences("\\", line);
+
+        return defaultIncrease + quotes + backslashes;
+      })
+      .Sum();
+  
+
+  private static int CountOccurrences(string search, string target)
+  {
+    var index = target.IndexOf(search, StringComparison.InvariantCulture);
+    var count = 0;
+
+    while (index >= 0)
+    {
+      count++;
+      index = target.IndexOf(search, index + 1, StringComparison.InvariantCulture);
+    }
+    
+    return count;
+  }
+}
