ec1a7a2311414a84662322740a09c93ab5e613a4
Robin de Gier <de.gier.robin@gmail.com>
2025-10-23 20:03:20 +0200

    feat: 2015 day 12

---
+++ dotnet/AoC/AoC2015.Tests/Day12Tests.cs
@@ -0,0 +1,45 @@
+﻿namespace AoC2015.Tests;
+
+public class Day12Tests
+{
+  [Theory]
+  [InlineData("[1,2,3]", 6)]
+  [InlineData("{\"a\":2,\"b\":4}", 6)]
+  [InlineData("[[[3]]]", 3)]
+  [InlineData("{\"a\":{\"b\":4},\"c\":-1}", 3)]
+  [InlineData("{\"a\":[-1,1]}", 0)]
+  [InlineData("[-1,{\"a\":1}] ", 0)]
+  [InlineData("[]", 0)]
+  [InlineData("{}", 0)]
+  public void PartOneTest(string input, int expected)
+  {
+    // Arrange
+    // Act
+    var result = Day12.PartOne([input]);
+    
+    // Assert
+    Assert.Equal(expected, result);
+  }
+  
+  [Theory]
+  [InlineData("[1,2,3]", 6)]
+  [InlineData("{\"a\":2,\"b\":4}", 6)]
+  [InlineData("[[[3]]]", 3)]
+  [InlineData("{\"a\":{\"b\":4},\"c\":-1}", 3)]
+  [InlineData("{\"a\":[-1,1]}", 0)]
+  [InlineData("[-1,{\"a\":1}] ", 0)]
+  [InlineData("[]", 0)]
+  [InlineData("{}", 0)]
+  [InlineData("[1,{\"c\":\"red\",\"b\":2},3]", 4)]
+  [InlineData("{\"d\":\"red\",\"e\":[1,2,3,4],\"f\":5}", 0)]
+  [InlineData("[1,\"red\",5]", 6)]
+  public void PartTwoTest(string input, int expected)
+  {
+    // Arrange
+    // Act
+    var result = Day12.PartTwo([input]);
+    
+    // Assert
+    Assert.Equal(expected, result);
+  }
+}
---
+++ dotnet/AoC/AoC2015/Day12.cs
@@ -0,0 +1,73 @@
+﻿using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Text.RegularExpressions;
+
+namespace AoC2015;
+
+public static partial class Day12
+{
+  public static void Run()
+  {
+    var lines = File.ReadAllLines("input/day12.txt");
+
+    Console.WriteLine($"Part one: {PartOne(lines)}");
+    Console.WriteLine($"Part two: {PartTwo(lines)}");
+  }
+
+  public static int PartOne(string[] input) =>
+    NumbersRegex()
+      .Matches(input[0])
+      .Aggregate(0, (current, match) => current + int.Parse(match.Value));
+
+  public static int PartTwo(string[] input)
+  {
+    const string forbiddenValue = "red";
+    var json = JsonNode.Parse(input[0])!;
+
+    var nodeStack = new Stack<JsonNode>();
+    nodeStack.Push(json);
+
+    var total = 0;
+
+    while (nodeStack.TryPop(out var node))
+    {
+      switch (node)
+      {
+        case JsonValue nodeValue when nodeValue.TryGetValue(out int numberValue):
+          total += numberValue;
+          break;
+        case JsonObject nodeObject:
+          {
+            var hasRed = nodeObject
+              .Select(x => x.Value)
+              .Where(x => x is JsonValue value && value.GetValueKind() == JsonValueKind.String)
+              .Any(x => string.Equals(forbiddenValue, x!.AsValue().ToString()));
+
+            if (hasRed)
+              break;
+        
+            foreach (var childNode in nodeObject.Where(x => x.Value is not null))
+            {
+              nodeStack.Push(childNode.Value!);
+            }
+
+            break;
+          }
+        case JsonArray nodeArray:
+          {
+            foreach (var childNode in nodeArray.Where(x => x is not null))
+            {
+              nodeStack.Push(childNode!);
+            }
+
+            break;
+          }
+      }
+    }
+    
+    return total;
+  }
+
+  [GeneratedRegex(@"(\d+|-\d+)")]
+  private static partial Regex NumbersRegex();
+}
